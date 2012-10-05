a9fad2a845c416fdc07d7cb070ea6481edecd231
yelena.shapovalova@hotmail.com <P@ssw0rd1984>
2012-10-05 09:24:10 +0300

    Google OAuth Process Add

---
+++ EbookZone/Ebook_Fan_Zone_1_0/EbookZone.Web/Controllers/AuthorizeController.cs
@@ -0,0 +1,72 @@
+﻿using System.Web.Mvc;
+using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OpenId;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
+using DotNetOpenAuth.OpenId.RelyingParty;
+
+namespace EbookZone.Web.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        private static readonly OpenIdRelyingParty openIdProvider = new OpenIdRelyingParty();
+
+        public ActionResult Index(string userOpenId)
+        {
+            // Response from provider
+            IAuthenticationResponse response = openIdProvider.GetResponse();
+
+            if (response == null)
+            {
+                Identifier id;
+
+                if (Identifier.TryParse(userOpenId, out id))
+                {
+                    try
+                    {
+                        IAuthenticationRequest request = openIdProvider.CreateRequest(userOpenId);
+                        FetchRequest fetch = new FetchRequest();
+                        fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.Contact.Email, true));
+                        fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.Name.First, true));
+                        fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.Name.Last, true));
+                        fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.BirthDate.WholeBirthDate, true));
+                        request.AddExtension(fetch);
+
+                        return request.RedirectingResponse.AsActionResult();
+                    }
+                    catch (ProtocolException ex)
+                    {
+                        TempData["error"] = ex.Message;
+                    }
+                }
+
+                return RedirectToAction("Index", "Login");
+            }
+
+            switch (response.Status)
+            {
+                case AuthenticationStatus.Authenticated:
+                    {
+                        //var fetches = response.GetExtension<FetchResponse>(); - fetches - get google account informations
+
+                        TempData["id"] = response.ClaimedIdentifier;
+                        return RedirectToAction("Index", "Home");
+                    }
+                case AuthenticationStatus.Canceled:
+                    {
+                        TempData["message"] = "Authentification was cancelled";
+                        return RedirectToAction("Index", "Login");
+                    }
+                case AuthenticationStatus.Failed:
+                    {
+                        TempData["error"] = response.Exception.Message;
+                        TempData["message"] = "Authentification was failed";
+                        return RedirectToAction("Index", "Login");
+                    }
+                default:
+                    {
+                        return RedirectToAction("Index", "Login");
+                    }
+            }
+        }
+    }
+}
--- EbookZone/Ebook_Fan_Zone_1_0/EbookZone.Web/Controllers/LoginController.cs
+++ EbookZone/Ebook_Fan_Zone_1_0/EbookZone.Web/Controllers/LoginController.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
-using DotNetOpenAuth.Messaging;
-using DotNetOpenAuth.OpenId;
-using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
-using DotNetOpenAuth.OpenId.RelyingParty;
 
 namespace EbookZone.Web.Controllers
 {
-    public class HomeController : Controller
+    public class LoginController : Controller
     {
+        //
+        // GET: /Login/
+
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
