c98d92634f9dd31ae544b5f2198b002175030ee3
Elena Shapovalova <Elena.Shapovalova@intego-group.com>
2012-10-22 19:30:31 +0300

    Auto increment id key

--- src/EbookZone.Domain/Base/BaseEntity.cs
+++ src/EbookZone.Domain/Base/BaseEntity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EbookZone.Domain.Base
 {
@@ -7,6 +9,7 @@ namespace EbookZone.Domain.Base
         private DateTime? _createDate;
         private DateTime? _updateDate;
 
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public DateTime? CreateDate
